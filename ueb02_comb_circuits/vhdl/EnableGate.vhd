9eb71f9a0c15b140f57cedd01df0a01e5ee5cc44
Jonas Arnold <jonas.arnold@live.de>
2022-03-19 09:08:08 +0100

    update to solution of sw2

--- ueb02_comb_circuits/vhdl/EnableGate.vhd
+++ ueb02_comb_circuits/vhdl/EnableGate.vhd
@@ -35,3 +35,77 @@ begin
   end process;
 end architecture;
 
+-- concurrent signal assignment (a bit more clever)
+architecture A_conc_sig_ass_2 of EnableGate is
+begin
+  y <= x and (en & en & en & en);
+end architecture;
+
+-- process statement with seq. sig. ass. (a bit more clever)
+architecture A_proc_seq_sig_ass_2 of EnableGate is
+begin
+  process(x, en)
+  begin
+    y <= x and (en & en & en & en);
+  end process;
+end architecture;
+
+-- process statement with "for" loop (clever)
+architecture A_proc_for_loop of EnableGate is
+begin
+  process(x, en)
+  begin
+    for i in 0 to x'length-1 loop -- loop param i only visible
+      y(i) <= x(i) and en;        -- within loop
+    end loop;
+  end process;
+end architecture;
+
+-- conditional signal assignment (clever)
+architecture A_cond_sig_ass of EnableGate is
+begin
+  y <= x when en = '1' else (others => '0');
+end architecture;
+
+-- process statem. replacing cond. signal ass. (clever)
+architecture A_proc_cond_sig_ass of EnableGate is
+begin
+  process(x, en)
+  begin
+    if en = '1' then
+      y <= x;
+    else
+      y <= (others => '0');
+    end if;
+  end process;
+end architecture;
+
+-- process with case statement (clever)
+architecture A_proc_with_case of EnableGate is
+begin
+  process(x, en)
+  begin
+    case en is
+      when '1' =>
+        y <= x;
+      when others =>
+        y <= (others => '0');
+    end case;
+  end process;
+end architecture;
+
+-- selected signal assignment (clever)
+architecture A_sel_sig_ass of EnableGate is
+begin
+  with en select
+    y <= x               when '1',
+         (others => '0') when others;
+end architecture;
+
+-- concurrent signal assignment with generate loop (expert)
+architecture A_generate_loop of EnableGate is
+begin
+  gen: for i in 0 to x'length-1 generate
+    y(i) <= x(i) and en;
+  end generate;
+end architecture;
--- ueb02_comb_circuits/vhdl/ParityGenerator.vhd
+++ ueb02_comb_circuits/vhdl/ParityGenerator.vhd
@@ -2,6 +2,10 @@
 -- Odd-Parity Generator
 -- Output parity complements input data such that the total number of '1' in
 -- data and parity is an odd number.
+--
+-- Note: The three different architectures described below all result in the
+-- same logic and thus have identical HW complexity (in this case 1 LUT,
+-- because 4-input combinational circuit).
 -------------------------------------------------------------------------------
 library ieee;
 use ieee.std_logic_1164.all;
@@ -13,15 +17,65 @@ entity ParityGenerator is
     );
 end ParityGenerator;
 
-architecture xor_chain of ParityGenerator is
-    
-    signal X : std_logic;    -- Result uf first xor
-    signal Y : std_logic;    -- Result uf first xor
-
+-------------------------------------------------------------------------------
+-- implementation with explicit one-counter
+-------------------------------------------------------------------------------
+architecture one_cnt of ParityGenerator is
 begin
+  
+  P_one_cnt: process(data)
+    variable onesCount : integer range data'length downto 0;
+  begin
+    onesCount := 0; -- initialize variable
+    for i in data'length-1 downto 0 loop
+      if data(i) = '1' then
+        onesCount := onesCount+1;
+      end if;
+    end loop;
+    if onesCount mod 2 = 0 then
+      parity <= '1';
+    else
+      parity <= '0';
+    end if;
+  end process P_one_cnt;
+ 
+end architecture one_cnt;
 
-    X <= data(0) xor data(1);
-    Y <= data(2) xor data(3);
-    parity <= not (X xor Y);
+-------------------------------------------------------------------------------
+-- implementation with incremental parity generation
+-------------------------------------------------------------------------------
+architecture incr_not of ParityGenerator is
+begin
+  
+  P_incr_not: process(data)
+    variable v_oddpar : std_logic;
+  begin
+    v_oddpar := '1'; -- assume data contains no '1'
+    for i in data'length-1 downto 0 loop
+      if data(i) = '1' then
+        v_oddpar := not v_oddpar;
+      end if;
+    end loop;
+    parity <= v_oddpar;
+  end process P_incr_not;
+  
+end architecture incr_not;
 
-end xor_chain;
+-------------------------------------------------------------------------------
+-- implementation with XOR chain
+-------------------------------------------------------------------------------
+architecture xor_chain of ParityGenerator is
+begin
+  
+  P_xor_chain: process(data)
+    variable v_xor : std_logic;
+  begin
+    v_xor := '1'; -- set first XOR input to '1'
+    for i in data'length-1 downto 0 loop
+      v_xor := v_xor xor data(i);
+    end loop;
+    parity <= v_xor;
+  end process P_xor_chain;
+ 
+end architecture xor_chain;
+
